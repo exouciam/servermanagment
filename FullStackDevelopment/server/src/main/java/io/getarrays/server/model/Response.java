1b497708490283f7a303c1ee854e657adc9d1acf
Exoucia M <63007560+exouciam@users.noreply.github.com>
2023-02-14 00:01:38 +0100

    Implement ServiceServerController

--- FullStackDevelopment/server/src/main/java/io/getarrays/server/model/Response.java
+++ FullStackDevelopment/server/src/main/java/io/getarrays/server/model/Response.java
@@ -1,8 +1,23 @@
 package io.getarrays.server.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.experimental.SuperBuilder;
+import org.springframework.http.HttpStatus;
+
 import java.time.LocalDateTime;
+import java.util.Map;
 
+@Data
+@SuperBuilder
+@JsonInclude(JsonInclude.Include.NON_NULL) // fields are not allowed to be null
 public class Response {
-
-    protected LocalDateTime timeStamp; //38:32
+    // send response back to user, after they sent a request
+    protected LocalDateTime timeStamp;
+    protected int statusCode;
+    protected HttpStatus status;
+    protected String reason;
+    protected  String message;
+    protected String developerMessage;
+    protected Map<?,?> data;
 }
